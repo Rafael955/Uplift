d4e0e09848b6a6d7eb583b36bb13d982bf965af7
Rafael Ferreira <30668559+Rafael955@users.noreply.github.com>
2020-07-03 14:47:39 -0300

    Criação da classe de extensão SessionExtension para serializar/deserializar sessions.

---
+++ src/Uplift.UI/Extensions/SessionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Uplift.UI.Extensions
+{
+    public static class SessionExtensions
+    {
+        /// <summary>
+        /// Método para passar um objeto session e o serializar.
+        /// </summary>
+        /// <param name="session"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public static void SetObject(this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+
+        /// <summary>
+        /// Método para recuperar um objeto deserializado de um objeto session serializado.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="session"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static T GetObject<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            return value.Equals(true) ? default : JsonConvert.DeserializeObject<T>(value);
+        }
+    }
+}
+// Classe simples com métodos de extensão aonde armazenamos um objeto o serializando e recuperamos o mesmo deserializando ele.
